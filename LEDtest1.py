686bfd7f6ecd11e43712dc67dfb9fb67f83defbb
SerchForVideos <serchnh17@hotmail.com>
2016-06-14 13:43:24 -0500

    X4

--- LEDtest1.py
+++ LEDtest1.py
@@ -1,14 +1,14 @@
 from ev3dev import *
 
 def main():
-    while(True)
-        if(Button.left())
+    while(True):
+        if(Button.left()):
             Leds.set(LEFT, GREEN)
-        else
+        else:
             LEds.set(LEFT, RED)
-        if(Button.right())
+        if(Button.right()):
             Leds.set(RIGHT, GREEN)
-        else
+        else:
             LEds.set(RIGHT, RED)
 
 if __name__ == '__main__':
