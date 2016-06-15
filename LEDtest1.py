152f5241c1b8cdec60b5759e181ce48625b0375b
SerchForVideos <serchnh17@hotmail.com>
2016-06-15 13:55:05 -0500

    X20

--- LEDtest1.py
+++ LEDtest1.py
@@ -1,5 +1,6 @@
 import ev3dev.ev3 as ev3
 import ev3dev.core as core
+import time as time
 
 def main():
     LEFT=ev3.Leds.LEFT
@@ -8,11 +9,12 @@ def main():
     RED=ev3.Leds.RED
     ev3.Leds.all_off()
     while(True):
-        core.Screen.clear()
+        '''core.Screen.clear()
         core.Screen.draw.text([5,20,200,100],String(ev3.Button.LEFT))
         core.Screen.draw.text([5,50,200,100],String(ev3.Button.RIGHT))
-        core.Screen.update()
-
+        core.Screen.update()'''
+        time.sleep(1)
+        print(ev3.Button.left+" "+ev3.Button.right)
         if(ev3.Button.left==True):
             ev3.Leds.set_color(LEFT, GREEN)
         else:
