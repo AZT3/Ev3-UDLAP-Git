de9bac005fa186e4fafaebc886d77e8febb0564f
SerchForVideos <serchnh17@hotmail.com>
2016-06-15 12:48:44 -0500

    X9

--- LEDtest1.py
+++ LEDtest1.py
@@ -1,15 +1,16 @@
 import ev3dev.ev3 as ev3
+import ev3dev.core as core
 
 def main():
     while(True):
         if(ev3.Button.left==True):
-            ev3.core.Leds.set(LEFT, GREEN)
+            core.Leds.set(LEFT, GREEN)
         else:
-            ev3.core.Leds.set(LEFT, RED)
+            core.Leds.set(LEFT, RED)
         if(ev3.Button.right==True):
-            ev3.core.Leds.set(RIGHT, GREEN)
+            core.Leds.set(RIGHT, GREEN)
         else:
-            ev3.core.Leds.set(RIGHT, RED)
+            core.Leds.set(RIGHT, RED)
 
 if __name__ == '__main__':
     main()
