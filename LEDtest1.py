382552db218546a6371014ee6135733fed0f3622
SerchForVideos <serchnh17@hotmail.com>
2016-06-20 12:41:45 -0500

    X25

--- LEDtest1.py
+++ LEDtest1.py
@@ -3,10 +3,11 @@ import ev3dev.core as core
 import time as time
 
 def main():
-    LEFT=ev3.Leds.LEFT
-    RIGHT=ev3.Leds.RIGHT
-    GREEN=ev3.Leds.GREEN
-    RED=ev3.Leds.RED
+    LEFT       =ev3.Leds.LEFT
+    RIGHT      =ev3.Leds.RIGHT
+    GREEN      =ev3.Leds.GREEN
+    RED        =ev3.Leds.RED
+    buttons    =Button()
     ev3.Leds.all_off()
     while(True):
         '''core.Screen.clear()
@@ -14,15 +15,17 @@ def main():
         core.Screen.draw.text([5,50,200,100],String(ev3.Button.RIGHT))
         core.Screen.update()'''
         time.sleep(1)
-        print(str(bool(ev3.Button.left))+" "+str(bool(ev3.Button.right)))
-        if(bool(ev3.Button.left)==True):
+        left_flag=buttons.left()
+        right_flag=buttons.right()
+        print(str(left_flag)+" "+str(right_flag))
+        '''if(left_flag==True):
             ev3.Leds.set_color(LEFT, GREEN)
         else:
             ev3.Leds.set_color(LEFT, RED)
-        if(bool(ev3.Button.right)==True):
+        if(right_flag==True):
             ev3.Leds.set_color(RIGHT, GREEN)
         else:
-            ev3.Leds.set_color(RIGHT, RED)
+            ev3.Leds.set_color(RIGHT, RED)'''
 
 if __name__ == '__main__':
     main()
